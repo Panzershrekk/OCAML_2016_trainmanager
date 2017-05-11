9f1e7f84fdb5f91aa8ff51f8c8024017bf86202f
Fossaert Thomas <thomas.fossaert@epitech.eu>
2017-05-11 21:01:20 +0200

    date

--- date.ml
+++ date.ml
@@ -1,14 +1,29 @@
 module type DATE =
   sig
+    val new_day : int
+    val new_month : int
+    val new_year : int
+
+    val calculate_day : int -> int -> int -> int -> string
+    val calculate_month : int -> int -> int -> int -> string
+    val calculate_year : int -> int -> int -> int -> string
+
     val is_leap_year : int -> bool
     val nb_days : int -> int -> int
     val string_to_int_cmp : string -> int -> int
     val verify_date : string list -> bool
     val date_is_valid : string -> bool
+    val calculate_date_with_int : int -> int -> int -> int -> string
+    val calculate_date : string list -> int -> string
   end
 
 module Date : DATE =
   struct
+
+  let new_day = 0;;
+  let new_month = 0;;
+  let new_year = 0;;
+
   let is_leap_year date =
     if date mod 4 = 0 && date mod 100 <> 0 || date mod 400 = 0 then true else false;;
 
@@ -41,4 +56,26 @@ module Date : DATE =
 
   let date_is_valid date = verify_date (Str.split (Str.regexp "-") date);;
 
+  let calculate_day day month year add = if (day + add) > (nb_days year month) then "01" else (if day + add < 10 then "0" ^ (string_of_int (day + add)) else string_of_int (day + add))
+
+  let calculate_month day month year add = if ((day + add) > (nb_days year month) && (month + 1) > 12 ) then "01" else (if month < 10 then "0" ^ (string_of_int (month)) else string_of_int (month))
+
+  let calculate_year day month year add = if month = 12 && (calculate_month day month year add) = "01" then string_of_int (year + 1) else string_of_int year
+
+
+  let calculate_date_with_int day month year add  = (calculate_day day month year add) ^ "-" ^ (calculate_month day month year add) ^ "-" ^ (calculate_year day month year add)
+
+(* let calculate_date_with_int day month year add = let new_day =
+                                                   if day + add > (nb_days year month) then (*day + add - (nb_days year month) *) "yolo" in
+                                                   let new_month = if new_day < day then month + 1 in
+                                                   let new_year = if new_month > 12 then 1 in
+                                                      ((string_of_int day) ^ "-"
+                                                      ^ (string_of_int new_month) ^
+                                                      "-" (string_of_int new_year))*)
+
+  let calculate_date date add = calculate_date_with_int
+                                (string_to_int_cmp (List.nth date 0) ~-1)
+                                (string_to_int_cmp (List.nth date 1) ~-1)
+                                (string_to_int_cmp (List.nth date 2) ~-1)
+                                add;;
 end;;
--- date.mli
+++ date.mli
@@ -1,9 +1,20 @@
 module type DATE =
   sig
+    val new_day : int
+    val new_month : int
+    val new_year : int
+
+    val calculate_day : int -> int -> int -> int -> string
+    val calculate_month : int -> int -> int -> int -> string
+    val calculate_year : int -> int -> int -> int -> string
+
     val is_leap_year : int -> bool
     val nb_days : int -> int -> int
     val string_to_int_cmp : string -> int -> int
     val verify_date : string list -> bool
     val date_is_valid : string -> bool
+
+    val calculate_date_with_int : int -> int -> int -> int -> string
+    val calculate_date : string list -> int -> string
   end
 module Date : DATE
